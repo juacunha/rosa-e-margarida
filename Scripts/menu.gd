3218cf92eec732e62311e2e78098562e19db0904
Debora Carvalho <dbndy3@gmail.com>
2025-06-30 01:21:27 -0300

    ✨: Added sair menu

--- Scripts/menu.gd
+++ Scripts/menu.gd
@@ -16,7 +16,6 @@ func _on_iniciar_2_pressed() -> void:
 	# Evita múltiplos cliques durante a transição
 	if is_transitioning:
 		return
-	
 	is_transitioning = true
 	
 	# Cria fade out suave
@@ -27,14 +26,22 @@ func _on_iniciar_2_pressed() -> void:
 	
 	# Espera o fade out terminar antes de mudar de cena
 	await fade_out_tween.finished
-	
 	# Emite o sinal para mudar de cena
 	change_scene.emit("Quarto")
-	
 	# Libera a cena atual
 	queue_free()
 
-# Opcional: Garantir que a música pare se a cena for destruída abruptamente
+func _on_sair_pressed() -> void:
+	 # Fade out da música antes de sair
+	var fade_out_tween := create_tween()
+	fade_out_tween.tween_property(ost_sound, "volume_db", -40.0, 1.0) \
+	.set_trans(Tween.TRANS_SINE) \
+	.set_ease(Tween.EASE_IN_OUT)
+	
+	await fade_out_tween.finished
+	get_tree().quit() 
+
+#Garante que a música pare se a cena for destruída abruptamente
 func _exit_tree():
 	if ost_sound.playing:
 		ost_sound.stop()
