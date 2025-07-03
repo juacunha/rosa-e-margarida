07a4d4910a84b716b0eaa9c3f6b6bffdb612c151
juacunha <juhaacunha@gmail.com>
2025-07-03 19:24:41 -0300

    versao final

--- Scripts/creditos.gd
+++ Scripts/creditos.gd
@@ -4,6 +4,8 @@ signal change_scene()
 
 @onready var hover = $hover
 @onready var click = $click
+@onready var petala_1 = $"petala/petala 1"
+@onready var petala_2 = $"petala 2/petala 2"
 
 func _on_menu_pressed():
 	click.play()
@@ -18,3 +20,7 @@ func _on_menu_mouse_entered():
 
 func _on_sair_mouse_entered():
 	hover.play()
+
+func _on_animation_player_animation_finished(zoom):
+	petala_1.play("petala caindo")
+	petala_2.play("caindo")
